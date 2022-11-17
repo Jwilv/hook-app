eebe47d09d7992ba67d8a3b23a9176dab5b9f8a3
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-17 16:15:59 -0300

    correcto uso del useState

--- src/components/01-usestate/CounterApp.js
+++ src/components/01-usestate/CounterApp.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './counterApp.css'
 
 export const CounterApp = () => {
+    const [ state, setState] = useState({
+        counter1: 20,
+        counter2: 30,
+        counter3: 50,
+        counter4: 80,
+    })
+    const {counter1,counter2} = state
     return (
         <>
-            <h1>{0}</h1>
+            <h1>{counter1}</h1>
+            <h1>{counter2}</h1>
             <hr />
-            <button className='btn btn-primary'>+1</button>
+            <button className='btn btn-primary'
+                onClick={() => {
+                    setState({
+                        ...state,
+                        counter1: counter1 + 1
+                    })
+                }} >
+                +1
+            </button>
         </>
     )
 }
