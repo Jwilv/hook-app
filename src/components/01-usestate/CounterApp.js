2804a7195f4f615abff35ba02ba2f954e0af9bcd
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-17 15:48:17 -0300

    creacion de <CounterApp /> y de  su estilado counterApp.css

---
+++ src/components/01-usestate/CounterApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './counterApp.css'
+
+export const CounterApp = () => {
+    return (
+        <>
+            <h1>{0}</h1>
+            <hr />
+            <button className='btn btn-primary'>+1</button>
+        </>
+    )
+}
