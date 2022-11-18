6613aed91e5363d9a625595677b67a9e3a667217
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-18 15:21:04 -0300

    se creo un formulario y se uso useEffect

--- src/components/02-useEffect/SimpleForm.js
+++ src/components/02-useEffect/SimpleForm.js
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './efects.css'
 
 
 export const SimpleForm = () => {
+
+    useEffect( () => {
+        console.log('se ejecuta una sola vez')
+    }, [])
+
+    const [stateForm, setStateForm] = useState({
+        emial: '',
+        name: '',
+    })
+
+    const { emial, name } = stateForm
+
+    const handleInputChange = ({ target }) => {
+        setStateForm({
+            ...stateForm,
+            [target.name]: target.value
+        })
+    }
+
+
     return (
         <>
             <h1>UseEffect</h1>
             <hr />
+            <div className='form-group'>
+                <input
+                    type='text'
+                    name='name'
+                    className='form-control'
+                    autoComplete='off'
+                    placeholder='tu nombre'
+                    value={name}
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div className='form-group'>
+                <input
+                    type='text'
+                    name='email'
+                    className='form-control'
+                    autoComplete='off'
+                    placeholder='email@email.com'
+                    value={emial}
+                    onChange={handleInputChange}
+                />
+            </div>
         </>
     )
 }
+
