8f445ae9f926b642ec55e87cca919b9e536692c8
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-18 16:23:43 -0300

    explicacion de useEfect en <Message />

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -3,9 +3,11 @@ import React, { useEffect } from 'react'
 export const Message = () => {
     useEffect( () => {
         console.log('componente montado')
+        //funcion que se ejecuta sola la primera vez que se renderizaz el componente, debido al []
 
         return () => {
             console.log('componente desmontado')
+            //aca se deberia usar un remove que desabilite la funcion anterior
         }
     }, [])
 
