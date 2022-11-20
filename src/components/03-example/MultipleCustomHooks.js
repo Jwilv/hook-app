0f50bd8b9882300361901a062d0bb29971e34bb8
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-20 00:59:27 -0300

    se agrego la api de breaking y funciona correctamente, se utilizaron operadores ternarios

--- src/components/03-example/MultipleCustomHooks.js
+++ src/components/03-example/MultipleCustomHooks.js
@@ -5,12 +5,27 @@ import './efectsMultipleCustomHooks.css'
 
 export const MultipleCustomHooks = () => {
 
-        const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`)
-        console.log(state)
+    const { loading,data } = useFetch(`https://www.breakingbadapi.com/api/quotes/1`)
+
+    const {author,quote} = !!data && data[0]
 
     return (
         <>
             <h1>Custom Hooks!!!</h1>
+            <hr />
+
+            {
+                loading
+                    ?
+                    (<div className='alert alert-info text-center'> loading...</div>)
+                    :
+                    (<blockquote className='blockquote text-right'>
+                        <p className='mb-0'>{quote}</p>
+                        <footer className='blockquote'>{author}</footer>
+                    </blockquote>
+                    )
+            }
+
         </>
     )
 }
