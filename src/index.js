33520d6f55c1cd6904c142cc2021960818d08fd1
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-22 16:07:30 -0300

    uso de todo [estructura (state,action) y agregar]

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-//import { CallBackHook } from './components/06-memos/CallBackHook';
-import { Padre } from './components/07-tarea-memo/Padre';
-//import { MemoHook } from './components/06-memos/MemoHook';
-//import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-//import { MultipleCustomHooks } from './components/03-example/MultipleCustomHooks';
-//import { FocusScreen } from './components/04-useRef/FocusScreen';
-//import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-//import { Layout } from './components/05-LayoutEffect/Layout';
-//import { Memorize } from './components/06-memos/Memorize';
-//import { CounterApp } from './components/01-usestate/CounterApp';
-//import { CounterWithCoustomHook } from './components/01-usestate/CounterWithCoustomHook';
-//import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// //import { CallBackHook } from './components/06-memos/CallBackHook';
+// import { Padre } from './components/07-tarea-memo/Padre';
+// //import { MemoHook } from './components/06-memos/MemoHook';
+// //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// //import { MultipleCustomHooks } from './components/03-example/MultipleCustomHooks';
+// //import { FocusScreen } from './components/04-useRef/FocusScreen';
+// //import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// //import { Layout } from './components/05-LayoutEffect/Layout';
+// //import { Memorize } from './components/06-memos/Memorize';
+// //import { CounterApp } from './components/01-usestate/CounterApp';
+// //import { CounterWithCoustomHook } from './components/01-usestate/CounterWithCoustomHook';
+// //import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
 
-  <Padre />
+//   <Padre />
 
-);
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+import './components/08-useReducer/intro-reducer'
