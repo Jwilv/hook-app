1103b7340ed836f7164b64b99ad23f8dae9346b8
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-24 16:11:30 -0300

    creacion de form para la aplicacion de useReducer

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -4,25 +4,66 @@ import { todoReducer } from './todoReducer'
 
 export const TodoApp = () => {
 
-    const initialSate =[{
+    const initialSate = [{
         id: new Date().getTime(),
-        desc:'Aprender React',
+        desc: 'Aprender React',
         done: false,
     }]
 
-    const [ todos ] = useReducer(todoReducer,initialSate)
+    const [todos] = useReducer(todoReducer, initialSate)
 
-    console.log( todos )
+    console.log(todos)
 
     return (
         <>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({todos.length})</h1>
             <hr />
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>aca estoy</li>
-            </ul>
+
+            <div className='row'>
+
+                <div className='col-7'>
+
+                    <ul className='list-group list-group-flush'>
+                        {
+                            todos.map((todos, i) => (
+                                <li
+                                    key={todos.id}
+                                    className='list-group-item'
+                                >
+                                    <p className='text-center'>{i + 1}. {todos.desc}</p>
+                                    <button
+                                        className='btn btn-danger'
+                                    >
+                                        borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+
+                </div>
+
+                <div className='col-5'>
+                    <h4>Agregar todo</h4>
+                    <hr />
+
+                    <form>
+                        <input
+                            type='text'
+                            name='description'
+                            placeholder='aprender ...'
+                            autoComplete='off'
+                            className='form-control'
+                        />
+                    </form>
+                    <button
+                        className='btn btn-block btn-outline-primary mt-1 btn-block'
+                    >
+                        agreagar
+                    </button>
+                </div>
+
+            </div>
         </>
     )
 }
