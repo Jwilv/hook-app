3fe55d94f9ab88169a4f78fcc0f94cc05de0fe69
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-10 23:19:21 -0300

    pruebas de renderizado con mock

--- src/tests/components/03-examples/MultipleCustomHooks.test.js
+++ src/tests/components/03-examples/MultipleCustomHooks.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme'
 import { MultipleCustomHooks } from '../../../components/03-example/MultipleCustomHooks';
+import { useFetch } from '../../../hooks/useFetch';
+jest.mock('../../../hooks/useFetch')
 
 
 
 describe('pruebas en <MultipleCustomHooks /> ', () => { 
 
     test('debe de renderizarse correctamente ', () => {
+
+        useFetch.mockReturnValue({
+            data:null,
+            loading:true,
+            error:null,
+        })
         const wrapper = shallow(<MultipleCustomHooks />)
         expect(wrapper).toMatchSnapshot();
     });
