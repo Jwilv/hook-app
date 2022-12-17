62c5a81747227d9cace62184af4f582bf303adcb
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-17 20:31:41 -0300

    se creo test para TodoListItem

---
+++ src/tests/components/08-useReducer/components/TodoListItem.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme'
+import { TodoListItem } from '../../../../components/08-useReducer/components/TodoListItem';
+import { demoTodo } from '../../../fixtures/demoTodos';
+
+
+describe('test em el componente <TodoListItem />', () => {
+
+    const handleToggle = jest.fn();
+    const handleDelete = jest.fn();
+
+
+    const wrapper = shallow(<TodoListItem 
+        todo={demoTodo[0]}
+        i={0}
+        handleToggle={handleToggle}
+        handleDelete={handleDelete}
+        />)
+
+
+    test('debe de mostrarse corectamente ', () => {
+        expect( wrapper ).toMatchSnapshot();
+    });
+
+    test('debe de llamar la funcion handleDelete', () => {
+        
+        wrapper.find('button').simulate('click');
+        expect( handleDelete ).toHaveBeenCalledWith(1);
+
+    });
+
+    test('debe de llamar la funcion handleToggle', () => {
+        
+        wrapper.find('p').simulate('click');
+        expect( handleToggle ).toHaveBeenCalledWith(1);
+
+    });
+
+    test('debe de mostrar el texto corectamente', () => {
+
+        expect(wrapper.find('p').text()).toBe(`1. ${demoTodo[0].desc}`)
+    });
+})
