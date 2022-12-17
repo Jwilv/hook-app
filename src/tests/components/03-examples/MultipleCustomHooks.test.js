ab53e670296eee7d87fe5c2aab12ec2db2b4bce8
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-17 15:09:02 -0300

    test en MultipleCustomHook

--- src/tests/components/03-examples/MultipleCustomHooks.test.js
+++ src/tests/components/03-examples/MultipleCustomHooks.test.js
@@ -19,11 +19,23 @@ describe('pruebas en <MultipleCustomHooks /> ', () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    test('', () => {
+    test('debe de mostrar la informacion', () => {
         useFetch.mockReturnValue({
-            data: null,
-            loading: true,
+            data: [{
+                author:'messi',
+                quote:'anda pa ya bobo'
+            }],
+            loading: false,
             error: null,
         })
+
+        const wrapper = shallow(<MultipleCustomHooks />)
+
+        expect(wrapper).toMatchSnapshot();
+
+        expect( wrapper.find('.alert').exists() ).toBe( false );
+        expect( wrapper.find('.mb-0').exists() ).toBe( true );
+        expect( wrapper.find('footer').text().trim() ).toBe( 'messi' );
+
     });
 })
