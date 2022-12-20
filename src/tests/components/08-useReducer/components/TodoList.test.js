b776d70b7acfe6939dd2922407307f95de1e6bfb
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-19 22:45:40 -0300

    creacion dae test para <TodoLit />

---
+++ src/tests/components/08-useReducer/components/TodoList.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { TodoList } from '../../../../components/08-useReducer/components/TodoList';
+import { demoTodo } from '../../../fixtures/demoTodos';
+
+
+describe('pruebas en el componente <TodoList />', () => {
+
+    const handleToggle = jest.fn();
+    const handleDelete = jest.fn();
+
+    const wrapper = shallow(<TodoList
+        todos={demoTodo}
+        handleToggle={handleToggle}
+        handleDelete={handleDelete}
+    />)
+
+    test('debe de mostrarse correctamente ', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe de tener 2 <TodoListItem /> ', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+})
