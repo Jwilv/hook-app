223f78b9db4423bb3d767c8f7cca4f0fb1223bd9
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-20 01:11:36 -0300

    se agreagaron test de <TodoAdd />

--- src/tests/components/08-useReducer/components/TodoAdd.test.js
+++ src/tests/components/08-useReducer/components/TodoAdd.test.js
@@ -1,9 +1,39 @@
-
+import React from 'react'
+import { shallow } from 'enzyme'
+import { TodoAdd } from '../../../../components/08-useReducer/components/TodoAdd';
 
 
 describe('pruebas en <TodoAdd />', () => { 
 
-    test('should ', () => {
-        
+    const handleAddTodo = jest.fn();
+
+    const wrapper = shallow( <TodoAdd 
+        handleAddTodo={handleAddTodo}/> );
+
+    test('debe de mostrarse corectamente ', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('NO debe de llamar a handleAddTodo', () => {
+        //una manera nueva de simular el evento
+        const formSubmit = wrapper.find('form').prop('onSubmit')
+        formSubmit( {preventDefault(){} } );
+
+        expect( handleAddTodo ).toHaveBeenCalledTimes(0);
+    });
+
+    test('debe de llamar a la funcion handleAddTodo', () => {
+        const value = 'si se pudo'
+        wrapper.find('input').simulate('change',{
+            target:{
+                value,
+                name : 'description'
+            }
+        })
+
+        const formSubmit = wrapper.find('form').prop('onSubmit')
+        formSubmit( {preventDefault(){} } );
+
+        expect( handleAddTodo ).toHaveBeenCalledTimes(1);
     });
  })
