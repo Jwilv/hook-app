b36f239cc4fe7f33ef7fc0f28975eae83c3e6823
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-20 15:18:30 -0300

    se agreagarom test para <TodoApp />

--- src/tests/components/08-useReducer/TodoApp.test.js
+++ src/tests/components/08-useReducer/TodoApp.test.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import { TodoApp } from '../../../components/08-useReducer/TodoApp';
+import { act } from '@testing-library/react';
+import { demoTodo } from '../../fixtures/demoTodos'
 
-describe('Pruebas en <TodoApp />', () => { 
-
-    const wrapper = shallow( <TodoApp />);
-
+describe('Pruebas en <TodoApp />', () => {
+    
     test('debe de mostrarse correctamente', () => {
+        const wrapper = shallow(<TodoApp />);
         expect(wrapper).toMatchSnapshot();
     });
- })
+
+    test('debe de agregar un Todo', () => {
+        const wrapper =  mount(<TodoApp />)
+        act(() => {
+            wrapper.find('TodoAdd').prop('handleAddTodo')(demoTodo[0]);
+            wrapper.find('TodoAdd').prop('handleAddTodo')(demoTodo[1]);
+        })
+
+        expect(wrapper.find('h1').text().trim()).toBe('Tareas (2)')
+    });
+})
