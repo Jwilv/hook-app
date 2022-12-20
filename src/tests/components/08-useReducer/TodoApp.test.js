1afcc08dab61aedf317b9175598752b06821b2e2
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-20 15:30:36 -0300

    se agrego test para <TodoApp />

--- src/tests/components/08-useReducer/TodoApp.test.js
+++ src/tests/components/08-useReducer/TodoApp.test.js
@@ -5,19 +5,29 @@ import { act } from '@testing-library/react';
 import { demoTodo } from '../../fixtures/demoTodos'
 
 describe('Pruebas en <TodoApp />', () => {
-    
+
+    Storage.prototype.setItem - jest.fn( ()=>{} );
+
+    const wrapper = shallow(<TodoApp />);
     test('debe de mostrarse correctamente', () => {
-        const wrapper = shallow(<TodoApp />);
+        
         expect(wrapper).toMatchSnapshot();
     });
 
-    test('debe de agregar un Todo', () => {
-        const wrapper =  mount(<TodoApp />)
-        act(() => {
-            wrapper.find('TodoAdd').prop('handleAddTodo')(demoTodo[0]);
-            wrapper.find('TodoAdd').prop('handleAddTodo')(demoTodo[1]);
-        })
+    // test('debe de agregar un Todo', () => {
+    //     const wrapper =  mount(<TodoApp />)
+    //     act(() => {
+    //         wrapper.find('TodoAdd').prop('handleAddTodo')(demoTodo[0]);
+    //         wrapper.find('TodoAdd').prop('handleAddTodo')(demoTodo[1]);
+    //     })
 
-        expect(wrapper.find('h1').text().trim()).toBe('Tareas (2)')
+    //     expect(wrapper.find('h1').text().trim()).toBe('Tareas (2)')
+    //     expect( localStorage.setItem ).toHaveBeenCalledTimes(2);
+    // });
+
+    test('debe de eliminar un todo ', () => {
+        wrapper.find('TodoAdd').prop('handleAddTodo')(demoTodo[0])
+        wrapper.find('TodoList').prop('handleDelete')(demoTodo[0].id)
+        expect(wrapper.find('h1').text().trim()).toBe('Tareas (0)')
     });
 })
